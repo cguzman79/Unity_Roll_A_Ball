7551b986dd8adc8f4e63c2ba122810916d058812
Chris Guzman <cguzman79@users.noreply.github.com>
2017-09-27 10:41:44 -0500

    Completed C# scripts

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	private Rigidbody rb;
+	public Text countText;
+	public Text winText;
+	private int count;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Red Pickup"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+		else if (other.gameObject.CompareTag ("Green Pickup"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 2;
+			SetCountText ();
+		}
+		else if (other.gameObject.CompareTag ("Gold Pickup"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 2;
+			SetCountText ();
+		}
+		else if (other.gameObject.CompareTag ("Special Pickup"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 12;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Total Score: " + count.ToString ();
+		if (count >= 100)
+		{
+			winText.text = "You Win!";
+		}
+	}
+
+
+}
